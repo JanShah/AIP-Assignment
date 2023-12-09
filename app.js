5278ea05c6e43babf13056f7e44af9d3d6b9984f
Jan Shah <shahcrew@gmail.com>
2023-12-09 21:16:05 +0000

    refinement and menu

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ class Agent {
         this.scale = config.fig_dim
     }
 
+    reScale(radius) {
+        this.radius = radius
+    }
+
 
     draw(ctx) {
         ctx.fillStyle = colours.agent.fill
@@ -57,25 +61,4 @@ async function start() {
 
 
 
-
-class Node {
-    constructor(x, y, cost, parent) {
-        this.x = x
-        this.y = y
-        this.cost = cost
-        this.parent = parent
-    }
-
-    equals(node, radius = -1) {
-        const dist = Math.sqrt((node.x - this.x) ** 2 + (node.y - this.y) ** 2)
-        if (radius > -1) {
-            
-            if (dist < radius/2) {
-                return true
-
-            }
-        }
-        return this.x === node.x && this.y === node.y
-    }
-}
 
